0cc8fabcfd629b5e3682d4b43a2fefdb924d4394
gdi2290 <github@gdi2290.com>
2015-06-10 00:58:10 -0700

    chore: update paths

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -6,19 +6,19 @@ import {bootstrap} from 'angular2/angular2';
 
 // include any injectables
 // import {routerInjectables} from 'angular2/router';
-import {routerInjectables} from 'patch_angular2/router';
+import {routerInjectables} from './patch_angular2/router';
 // App
-import {appServicesInjectables} from 'app/services/services';
+import {appServicesInjectables} from './app/services/services';
 // import {shadowDomInjectables} from 'common/shadowDomInjectables';
 // import {jitInjectables} from 'common/jitInjectables';
-import {falcorInjectibles} from 'common/FalcorModel';
+import {falcorInjectibles} from './common/FalcorModel';
 
-import {rxPipeRegistry} from 'common/rxPipeRegistry';
+import {rxPipeRegistry} from './common/rxPipeRegistry';
 
 // import {initialCache} from 'common/initialCache';
 
 // Top level component to bootstrap
-import {App} from 'app/App';
+import {App} from './app/App';
 
 // bootstrap the Angular app with bindings
 bootstrap(App, [
