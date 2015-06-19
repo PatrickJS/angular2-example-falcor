411b7ee215fd7912893715fa7b787f49bd15b604
gdi2290 <github@gdi2290.com>
2015-06-18 18:12:55 -0700

    refactor(FalcorModel): update XMLHttpSource

--- src/common/FalcorModel.ts
+++ src/common/FalcorModel.ts
@@ -3,6 +3,7 @@ import {bind, Inject} from 'angular2/di';
 import {Model} from 'falcor';
 import {XMLHttpSource} from 'falcor-browser';
 
+// import {initialCache} from './demoCache';
 import {initialCache} from './initialCache';
 
 var $ref = Model.ref;
@@ -13,12 +14,21 @@ class ModelRoot {
   unsafeMode:boolean = false;
 }
 
+export class AppSource extends XMLHttpSource {
+  constructor(@Inject('modelPath') path: string) {
+    super(path, {
+      headers: {},
+      timeout: 15000
+    })
+  }
+}
+
 export class FalcorModel extends Model {
-  constructor(@Inject('initialCache') cache) {
+  constructor(source: AppSource, @Inject('initialCache') cache) {
     super({
       cache: cache,
-      source: new XMLHttpSource('model.json', 15000),
-      root: new ModelRoot()
+      source: source,
+      root: new ModelRoot() // needed for imagine that need getValueSync
     });
   }
 }
@@ -26,6 +36,8 @@ export class FalcorModel extends Model {
 
 
 export var falcorInjectibles = [
+  bind('modelPath').toValue('/model.json'),
   bind('initialCache').toValue(initialCache),
-  bind(FalcorModel).toClass(FalcorModel)
+  AppSource,
+  FalcorModel
 ];
